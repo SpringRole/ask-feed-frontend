aef4a59d21527026672af878a21bc355baa96016
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2021-06-10 18:20:39 +0530

    Update Dashboard.js
    Basic setup of Dashboard, will get updated more.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,26 @@
+import React from "react";
+import logo from "./LandingPage/images/favicon.png";
+import "./Dashboard.css";
+import { Link } from "react-router-dom";
+
 const DashBoard = () => {
-  return <p>Hi this is DashBoard</p>;
+  return (
+    <>
+      <img src={logo} className="head-logo" />
+      <Link to="/surveys/new">
+        <button className="create-survey">Create a Survey</button>
+      </Link>
+      <button className="dashboard-btn">Dashboard</button>
+      <a className="logout">Logout</a>
+      <div className="footer"></div>
+      <img src={logo} className="footer-logo" />
+      <a className="create-survey-anchor">Create a Survey</a>
+      <a className="about-us">About Us</a>
+      <a className="contact-us">Contact Us</a>
+      <a className="pricing">Pricing Us</a>
+      <a className="terms">Terms of Use</a>
+    </>
+  );
 };
 
 export default DashBoard;
