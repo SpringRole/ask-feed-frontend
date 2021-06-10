2444c6953b1a93956586328879fa9c40c03482a4
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2021-06-10 18:19:35 +0530

    Create SurveyField.js

---
+++ src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+//this file will contain the logic to render a single label and text input
+import React from "react";
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
