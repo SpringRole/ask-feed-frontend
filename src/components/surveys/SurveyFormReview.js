ab5af7abf4d86f2503c0f8b22fad6a67a20beba5
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2021-06-15 18:35:05 +0530

    Create SurveyFormReview.js

---
+++ src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,34 @@
+import React from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import formFields from "./formFields";
+import { withRouter } from "react-router-dom";
+import * as actions from "../../actions/index";
+
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h5>Please comfirm your entries</h5>
+      {reviewFields}
+      <button onClick={onCancel}>Back</button>
+      <button onClick={() => submitSurvey(formValues, history)}>
+        Send Survey
+      </button>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
