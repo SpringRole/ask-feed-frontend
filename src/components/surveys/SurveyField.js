95f4e29e672a86eb0d110f713cfaca1f01cd55fa
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2021-06-16 21:02:38 +0530

    Update SurveyField.js

--- src/components/surveys/SurveyField.js
+++ src/components/surveys/SurveyField.js
@@ -3,10 +3,14 @@ import React from "react";
 export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
-      <label>{label}</label>
-      <input {...input} />
-      {touched && error}
+      <label style={{ fontSize: "35px", color: "#3362a8" }}>{label}</label>
+      <input {...input} style={{ marginBottom: "5px", fontSize: "30px" }} />
+      <div
+        className="red-text"
+        style={{ marginBottom: "20px", fontSize: "30px" }}
+      >
+        {touched && error}
+      </div>
     </div>
   );
 };
-
