43312c927bdfcb33626cf9db3633d2903cfb6ab6
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2021-06-17 15:07:08 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER, FETCH_SURVEYS } from "./types";
+import {  FETCH_SURVEYS } from "./types";
 
 export const submitSurvey = (values, history) => async (dispatch) => {
   const res = await axios.post(
