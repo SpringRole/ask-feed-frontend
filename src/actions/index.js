bb9cc0295bda73ec8876c1ba250a6f680a4d5f03
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2021-06-17 15:05:47 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,11 @@
 import axios from "axios";
 import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
-// export const fetchUser = () => async (dispatch) => {
-//   const res = axios.get("/api/current_user");
-//   dispatch({ type: FETCH_USER, payload: res.data });
-// };
-
 export const submitSurvey = (values, history) => async (dispatch) => {
   const res = await axios.post(
     "http://localhost:2000/survey/createsurvey",
     values
   );
-  history.push("/surveys");
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
