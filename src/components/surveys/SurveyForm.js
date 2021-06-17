6dde5ba89b58521dacdfbebdc43c7f7b5bf82dc1
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2021-06-17 15:18:09 +0530

    Update SurveyForm.js

--- src/components/surveys/SurveyForm.js
+++ src/components/surveys/SurveyForm.js
@@ -4,10 +4,12 @@ import { reduxForm, Field } from "redux-form";
 import SurveyField from "./SurveyField";
 import { Link } from "react-router-dom";
 import "./SurveyForm.css";
+import formFields from "./formFields";
+import validateEmails from "../utils/validateEmails";
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field
           key={name}
@@ -22,13 +24,29 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
+        <h3
+          style={{ marginBottom: "53px", fontWeight: "bold", color: "#3362a8" }}
+        >
+          Please provide the following details 👉🏼
+        </h3>
         <form
           className="text-form"
           onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}
         >
-          <Link>Cancel</Link>
           {this.renderFields()}
-          <button className="submit-btn" type="submit">
+          <Link
+            to="/dashboard"
+            className="red btn-flat white-text"
+            style={{ fontSize: "30px", height: "50px" }}
+          >
+            Cancel
+          </Link>
+
+          <button
+            className="teal btn-flat right white-text"
+            style={{ fontSize: "30px", height: "50px" }}
+            type="submit"
+          >
             Next
           </button>
         </form>
@@ -54,3 +72,4 @@ export default reduxForm({
   form: "surveyForm",
   destroyOnUnmount: false,
 })(SurveyForm);
+
